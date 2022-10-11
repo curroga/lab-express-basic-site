11a394fe63f7283b446464c0d515a2f771668ef3
Yago <yagoponcem@gmail.com>
2022-10-11 17:41:16 +0200

    todos los archivos funcionando

--- app.js
+++ app.js
@@ -8,6 +8,28 @@ app.get("/home", (req, res) => {
   res.sendFile(__dirname + "/views/home.html")
 })
 
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html")
+} )
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/fotos", (req, res) => {
+  res.sendFile(__dirname + "/views/foto-galeries.html")
+})
+
+
+
+
+
+app.get("*", (req, res) => {
+  res.send("No existe la página")
+})
+
 app.listen(port, () => {
   console.log("funciona")
 })
+
+
